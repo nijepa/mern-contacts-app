c5719335eb3fff1e5fb8509bb5e28cf21b84e814
Nikola Pavicevic <nijepa@gmail.com>
2019-10-08 23:11:08 +0200

    Setup Navbar component, Home and About pages

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About App</h1>
+            <p className="my-1">
+                Full Stack App (Node - Express - MongoDB - React) for contacts
+            </p>
+            <p className="bg-dark p">
+                <strong>Version 1.0.0</strong>
+            </p>
+        </div>
+    );
+};
+
+export default About;
